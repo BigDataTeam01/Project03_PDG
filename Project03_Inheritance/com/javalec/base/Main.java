ab0fabd8aee3271c4e23fbecceb8c70b10a6386f
ForrestDPark <pulpilisory@gmail.com>
2023-12-08 11:29:49 +0900

    addition issue sol ver 01

--- Project03_Inheritance/com/javalec/base/Main.java
+++ Project03_Inheritance/com/javalec/base/Main.java
@@ -42,7 +42,7 @@ public class Main {
 		
 		
 		
-		System.out.println(" >>> Field를 사용 ");
+		System.out.println("n >>> Field를 사용 ");
 		System.out.println(String.format(
 				 "1. 1번과 2번 의 숫자의 합은 %d 입니다.  ",
 				 calc.addAction2()));
@@ -59,7 +59,7 @@ public class Main {
 				));
 		// [Constructor 로 데이터 전달]
 		// Constructor의 파라미터에 숫자를 입력하여 생성되는 순간 output  print 가 되도록 함.  
-		System.out.println(" >>> Constructor 를 사용 ");
+		System.out.println("\n >>> Constructor 를 사용 ");
 		System.out.println("3번과 4번의 덧셈, 뺄셈, 곱셈의 결과는 ");
 		Addition  add  	= new Addition( calc.num3,calc.num4);
 		AddSub    sub 	= new AddSub(   calc.num3,calc.num4);
